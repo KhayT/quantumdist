99348e48c362cf1ca7b1ed0a6a631ae3fd90ba95
KhayT <tkprof4@gmail.com>
2022-05-09 12:05:21 -0700

    Quantum Dist files

---
+++ index.8d9bdcfd.js
@@ -0,0 +1,2 @@
+const mobileMenu=document.querySelector(".menu"),navUl=document.querySelector(".header__nav");mobileMenu.addEventListener("click",(function(){navUl.classList.toggle("show")}));
+//# sourceMappingURL=index.8d9bdcfd.js.map
